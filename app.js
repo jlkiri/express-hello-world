3fbfaf582fd89c383d2f869e52c0c4690fea3f49
jlkiri <interalia.dev@gmail.com>
2021-02-27 21:24:33 +0900

    Add redis

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require("express")
+const redis = require("redis")
+const client = redis.createClient()
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+client.on("error", function (err) {
+  console.log("Error " + err)
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+client.set("string key", "string val", redis.print)
+client.hset("hash key", "hashtest 1", "some value", redis.print)
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print)
+
+client.hkeys("hash key", function (err, replies) {
+  console.log(replies.length + " replies:")
+
+  replies.forEach(function (reply, i) {
+    console.log("    " + i + ": " + reply)
+  })
+
+  // client.quit()
+})
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.get("/", (req, res) => res.send(client.get("string key")))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
